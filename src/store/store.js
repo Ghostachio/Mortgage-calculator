42ea07d0216e30dfbbfef331b421b0e0127c87e6
Ghostachio <mansour.hamza2.0@gmail.com>
2024-09-16 18:37:25 +0100

    changed the name on the tab

--- src/store/store.js
+++ src/store/store.js
@@ -37,53 +37,6 @@ const calculateSlice = createSlice({
   },
 });
 
-// const calcReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "updateAmount":
-//       return {
-//         ...state,
-//         amount: action.payload,
-//       };
-//     case "updateTerm":
-//       return {
-//         ...state,
-//         term: action.payload,
-//       };
-//     case "updateRate":
-//       return {
-//         ...state,
-//         rate: action.payload,
-//       };
-//     case "updateType":
-//       return {
-//         ...state,
-//         type: action.payload,
-//       };
-//     case "updateMonthly":
-//       return {
-//         ...state,
-//         monthly: action.payload,
-//       };
-//     case "updateInterest":
-//       return {
-//         ...state,
-//         monthly: action.payload,
-//       };
-//     case "clearAll":
-//       return {
-//         ...state,
-//         amount: "",
-//         term: "",
-//         rate: "",
-//         type: "",
-//         monthly: "",
-//         total: "",
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
 const store = configureStore({
   reducer: {
     calculator: calculateSlice.reducer,
